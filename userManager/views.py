19b74df0fd5a2eaed3350fdfdca64ef254061c30
CastleLong911 <castlelong911@gmail.com>
2022-12-07 10:08:04 +0900

    login

--- userManager/views.py
+++ userManager/views.py
@@ -1,22 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from django.contrib.auth.models import User
+from django.contrib.auth import login as lg, authenticate
 # Create your views here.
 
 def login(request):
     return render(request, 'login.html', {})
 
-def signup(request):
+def signup_form(request):
     return render(request, 'signup.html', {})
+
+def signup(request):
+    if request.method == 'GET':
+        return render(request, 'index.html', {})
+    else:
+        username = request.POST['username']
+        password = request.POST['password']
+        user = User.objects.create_user(username, '', password)
+        user.save()
+        user = authenticate(request, username=username, password=password)
+        lg(request, user)
+        return redirect('index')
 
 def ajax_username_check(request):
     if request.method == 'GET':
         return render(request, 'index.html', {})
     else:
-        print('username check!')
         username = request.POST['username']
         try:
             temp = User.objects.get(username=username)
             return JsonResponse({'result': 'disable'})
         except User.DoesNotExist:
             return JsonResponse({'result' : 'available'})
+
